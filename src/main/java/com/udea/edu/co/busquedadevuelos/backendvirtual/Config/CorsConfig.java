13d37caf5ded2b0412dc5f9a785cbf1441e69c32
Juan Loaiza <juanmazo1234@gmail.com>
2024-04-23 12:43:58 -0500

    se agrega archivo configuracion para cors

---
+++ src/main/java/com/udea/edu/co/busquedadevuelos/backendvirtual/Config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.udea.edu.co.busquedadevuelos.backendvirtual.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Cambia esto por el origen permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Encabezados permitidos
+    }
+}
+
